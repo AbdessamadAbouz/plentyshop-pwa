cbd3006235ba3589cd7f1049acdc25b84d88baa6
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2025-08-12 15:11:49 +0100

    [MegaMenu] Show cheezit image + url redirect

--- apps/web/components/MegaMenu/MegaMenu.vue
+++ apps/web/components/MegaMenu/MegaMenu.vue
@@ -65,7 +65,7 @@
                     <SfListItem
                                 :tag="NuxtLink"
                                 size="sm"
-                                :href="localePath(generateCategoryLink(node))"
+                                :href="categoryTreeGetters.getName(node) == 'Cheezit' ? '/cheezit' : localePath(generateCategoryLink(node))"
                                 class="kl-mega-menu-item g-16-m flex flex-col mb-2 hover:bg-white"
                                 :id="categoryTreeGetters.getSlug(node)">
                       {{ categoryTreeGetters.getName(node) }}
