18b23764aea2587086fce51cf744f270414607d9
Abdessamad Abouzaid <abdessamad.abouz@gmail.com>
2025-11-26 16:31:35 +0100

    B2B/PWA: manufacturer/ EU representative

--- apps/web/components/ProductLegalDetailsDrawer/ProductLegalDetailsDrawer.vue
+++ apps/web/components/ProductLegalDetailsDrawer/ProductLegalDetailsDrawer.vue
@@ -58,32 +58,91 @@
         :aria-labelledby="`tab-${index}`"
         class="p-4"
       >
-        <component :is="tab.component" :product="product" />
+        <component :is="tab.component" :product="props.product" />
       </div>
     </SfDrawer>
   </UiOverlay>
 </template>
 
 <script setup lang="ts">
+import { computed, ref, watch } from 'vue';
 import type { SfDrawerPlacement } from '@storefront-ui/vue';
 import { SfDrawer, SfIconClose, useTrapFocus } from '@storefront-ui/vue';
+import { manufacturerGetters, productGetters } from '@plentymarkets/shop-api';
 import type { ProductLegalDetailsProps } from '~/components/ProductLegalDetailsDrawer/types';
 import ManufacturerResponsibleInfo from '~/components/ManufacturerResponsibleInfo/ManufacturerResponsibleInfo.vue';
 import ManufacturerInformation from '~/components/ManufacturerInformation/ManufacturerInformation.vue';
 
-defineProps<ProductLegalDetailsProps>();
+const props = defineProps<ProductLegalDetailsProps>();
 
 const { t } = useI18n();
 
 const placement = ref<`${SfDrawerPlacement}`>('right');
-const tabs = [
-  { label: t('manufacturer.euResponsibleTabName'), component: ManufacturerResponsibleInfo, disabled: false },
-  { label: t('manufacturer.manufacturerTabName'), component: ManufacturerInformation, disabled: false },
-];
+
+const manufacturer = computed(() => productGetters.getManufacturer(props.product));
+
+const responsibleCountry = computed(() => manufacturerGetters.getManufacturerResponsibleCountry(manufacturer.value));
+const responsibleInfo = computed(() => ({
+  name: manufacturerGetters.getManufacturerResponsibleName(manufacturer.value),
+  street: manufacturerGetters.getManufacturerResponsibleStreet(manufacturer.value),
+  houseNo: manufacturerGetters.getManufacturerResponsibleHouseNo(manufacturer.value),
+  postCode: manufacturerGetters.getManufacturerResponsiblePostCode(manufacturer.value),
+  town: manufacturerGetters.getManufacturerResponsibleTown(manufacturer.value),
+  country: Object.keys(responsibleCountry.value || {}).length > 0 ? responsibleCountry.value : null,
+  email: manufacturerGetters.getManufacturerResponsibleEmail(manufacturer.value),
+  phoneNo: manufacturerGetters.getManufacturerResponsiblePhoneNo(manufacturer.value),
+  responsibleContactUrl: manufacturerGetters.getManufacturerResponsibleContactUrl(manufacturer.value),
+}));
+
+const manufacturerCountry = computed(() => manufacturerGetters.getManufacturerCountry(manufacturer.value));
+const manufacturerInfo = computed(() => ({
+  logo: manufacturerGetters.getManufacturerLogo(manufacturer.value),
+  name: manufacturerGetters.getManufacturerName(manufacturer.value),
+  externalName: manufacturerGetters.getManufacturerExternalName(manufacturer.value),
+  legalName: manufacturerGetters.getManufacturerLegalName(manufacturer.value),
+  street: manufacturerGetters.getManufacturerStreet(manufacturer.value),
+  houseNo: manufacturerGetters.getManufacturerHouseNo(manufacturer.value),
+  postcode: manufacturerGetters.getManufacturerPostCode(manufacturer.value),
+  town: manufacturerGetters.getManufacturerTown(manufacturer.value),
+  country: Object.keys(manufacturerCountry.value || {}).length > 0 ? manufacturerCountry.value : null,
+  email: manufacturerGetters.getManufacturerEmail(manufacturer.value),
+  phoneNumber: manufacturerGetters.getManufacturerPhoneNumber(manufacturer.value),
+  faxNumber: manufacturerGetters.getManufacturerFaxNumber(manufacturer.value),
+  contactUrl: manufacturerGetters.getManufacturerContactUrl(manufacturer.value),
+  url: manufacturerGetters.getManufacturerUrl(manufacturer.value),
+}));
+
+const hasResponsibleInfo = computed(() => Object.values(responsibleInfo.value).some(Boolean));
+const hasManufacturerInfo = computed(() => Object.values(manufacturerInfo.value).some(Boolean));
+
+const tabs = computed(() => {
+  if (hasResponsibleInfo.value || hasManufacturerInfo.value) {
+    return [
+      {
+        label: t('manufacturer.combinedTabName'),
+        component: hasResponsibleInfo.value ? ManufacturerResponsibleInfo : ManufacturerInformation,
+        disabled: false,
+      },
+    ];
+  }
+  return [];
+});
 
 const activeTabIndex = ref(0);
 
-const isActiveTab = (index: number) => activeTabIndex.value === index;
+watch(
+  () => tabs.value.length,
+  (len) => {
+    if (len === 0) {
+      activeTabIndex.value = -1;
+    } else if (activeTabIndex.value >= len) {
+      activeTabIndex.value = 0;
+    }
+  },
+  { immediate: true },
+);
+
+const isActiveTab = (index: number) => activeTabIndex.value === index && tabs.value.length > 0;
 const setActiveTab = (index: number) => {
   activeTabIndex.value = index;
 };
